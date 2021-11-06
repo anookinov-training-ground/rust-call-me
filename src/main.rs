9ae414d40f7bd000eb4179fd7c16c41a5f187d02
Alexander Nookinov <anookinov@gmail.com>
2021-11-06 14:42:20 +0700

    add function items and function pointers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
+    let x = bar; // function item
+    let mut x = bar_generic::<i32>; // function item
+    println!("{}", std::mem::size_of_val(&x));
+    // x = bar_generic()::<u32>;
+    baz(bar_baz::<u32>);
+    baz(bar_baz::<i32>);
+}
+
+fn bar() {}
+
+fn bar_generic<T>() {}
+
+fn bar_baz<T>(_: u32) -> u32 {
+    0
+}
+
+fn baz(f: fn(u32) -> u32) {
+    println!("{}", std::mem::size_of_val(&f));
 }
